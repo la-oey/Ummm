182ddea9f062a3e1e810c21dc7e1ffa8c436074a
Lauren Oey <loey@ucsd.edu>
2019-01-08 15:27:51 -0800

    removes umms in text to train LM

--- writeTrainingTxt.py
+++ writeTrainingTxt.py
@@ -9,6 +9,7 @@ Created on Sun Dec  2 18:26:56 2018
 import time
 import sys
 import csv
+import re
 csv.field_size_limit(sys.maxsize)
 
 filenames = dict()
@@ -23,8 +24,16 @@ def main():
             if r['filename'] not in filenames:
                 filenames[r['filename']] = [r['filename']]
                 print(r['filename'])
+
+            newSentAsString = ""
+            if r['sentLength'] != None and any(re.match("^umm+$", x) for x in r['text'].split()):
+                newSent = []
+                for w in r['text'].split(): 
+                    if not re.match("^umm+$", w):
+                        newSent.append(w)
+                newSentAsString = " ".join(newSent)
             
-            training_file.write(r['text'] + "\n")
+            training_file.write(newSentAsString + "\n")
         training_file.close()
         csv_file_r.close()
     print("writeTrainingTxt.py Run Time: " + str(time.time()-start_time) + " seconds")
