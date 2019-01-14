7d244045ad4ec23e886b48b553c12126910cc35c
Lauren Oey <loey@ucsd.edu>
2019-01-14 14:41:25 -0800

    add validation and test sets for training lstm

--- splitData.py
+++ splitData.py
@@ -13,12 +13,16 @@ import random
 csv.field_size_limit(sys.maxsize)
 
 author_training = dict()
+author_training_v = dict()
+author_training_t = dict()
 author_validation = dict()
 author_testing = dict()
 filenames = dict()
 
 # proportion of data in each set
-p_train = 0.35
+p_train = 0.315
+p_train_v = 0.0175
+p_train_t = 0.0175
 p_validate = 0.35
 p_test = 0.30
 
@@ -26,6 +30,8 @@ def main():
     start_time = time.time()
     with open('preprocessed/processed_allFiles.csv', 'r') as csv_file_r:
         training_file = open('split/training_allFiles.csv', 'w')
+        training_valid_file = open('split/training_valid_allFiles.csv', 'w')
+        training_test_file = open('split/training_test_allFiles.csv', 'w')
         validation_file = open('split/validation_allFiles.csv', 'w')
         testing_file = open('split/testing_allFiles.csv', 'w')
         
@@ -33,6 +39,10 @@ def main():
         fieldnames = ['filename', 'author', 'subreddit', 'title', 'text', 'sentLength', 'timestamp']
         training = csv.DictWriter(training_file, fieldnames=fieldnames)
         training.writeheader()
+        training_v = csv.DictWriter(training_valid_file, fieldnames=fieldnames)
+        training_v.writeheader()
+        training_t = csv.DictWriter(training_test_file, fieldnames=fieldnames)
+        training_t.writeheader()
         validation = csv.DictWriter(validation_file, fieldnames=fieldnames)
         validation.writeheader()
         testing = csv.DictWriter(testing_file, fieldnames=fieldnames)
@@ -44,18 +54,31 @@ def main():
                 print(r['filename'])
             
             rowinfo = {'filename': r['filename'], 'author':r['author'], 'subreddit':r['subreddit'], 'title':r['title'], 'text':r['text'], 'sentLength':r['sentLength'], 'timestamp': r['timestamp']}
+            
             if r['author'] in author_training:
                 training.writerow(rowinfo)
+            elif r['author'] in author_training_v:
+                training_v.writerow(rowinfo)
+            elif r['author'] in author_training_t:
+                training_t.writerow(rowinfo)
             elif r['author'] in author_validation:
                 validation.writerow(rowinfo)
             elif r['author'] in author_testing:
                 testing.writerow(rowinfo)
             else:
                 rand = random.random()
+
+                
                 if rand < p_train:
                     author_training[r['author']] = [r['author']]
                     training.writerow(rowinfo)
-                elif rand < (p_train + p_validate):
+                elif rand < (p_train + p_train_v):
+                    author_training_v[r['author']] = [r['author']]
+                    training_v.writerow(rowinfo)
+                elif rand < (p_train + p_train_v + p_train_t):
+                    author_training_t[r['author']] = [r['author']]
+                    training_t.writerow(rowinfo)
+                elif rand < (p_train + p_train_v + p_train_t + p_validate):
                     author_validation[r['author']] = [r['author']]
                     validation.writerow(rowinfo)
                 else:
@@ -63,6 +86,8 @@ def main():
                     testing.writerow(rowinfo)
         csv_file_r.close()
         training_file.close()
+        training_valid_file.close()
+        training_test_file.close()
         validation_file.close()
         testing_file.close()
     print("splitData.py Run Time: " + str(time.time()-start_time) + " seconds")
